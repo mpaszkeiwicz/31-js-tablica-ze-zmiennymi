b40ba8eb2d2fdfbd84a1a3cf5776b187bd84e163
mpaszkeiwicz <m.paszkiewicz@i-propert.pl>
2022-04-11 10:17:09 +0200

    tablica

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("tablica-ze-zmiennymi");
+
+let liczba1 = 1;
+let liczba2 = 2;
+let liczba3 = liczba1 + 3 * liczba2;
+let tablicaLiczb = [liczba1, liczba2, liczba3];
+console.log(tablicaLiczb);
